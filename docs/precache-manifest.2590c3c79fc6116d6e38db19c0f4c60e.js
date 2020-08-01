c9a047c58d4fe660028dd24a65aecdc069d7e199
ariachuqui <ari.chuquiruna@gmail.com>
2020-08-01 14:45:07 -0300

    docs agregado

---
+++ docs/precache-manifest.2590c3c79fc6116d6e38db19c0f4c60e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6c69e20f3cbf77a11a3ebdd7f545b7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "976286727ecae3cafdb0",
+    "url": "/static/css/main.7d5d7c58.chunk.css"
+  },
+  {
+    "revision": "74a92ea2561df0b200ab",
+    "url": "/static/js/2.507d7a16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.507d7a16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "976286727ecae3cafdb0",
+    "url": "/static/js/main.6b7a2d33.chunk.js"
+  },
+  {
+    "revision": "39e88660e5d512526393",
+    "url": "/static/js/runtime-main.3c337214.js"
+  }
+]);
